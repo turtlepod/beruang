d79058dd3fc3f5439940cfa58304685c5fde8d5c
David <david.warna@gmail.com>
2026-06-22 23:03:19 +0700

    Fix BERUANG_VERSION constant to match package.json (0.5.0-beta)

--- beruang.php
+++ beruang.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'BERUANG_VERSION', '0.4.0-beta' );
+define( 'BERUANG_VERSION', '0.5.0-beta' );
 define( 'BERUANG_PLUGIN_FILE', __FILE__ );
 define( 'BERUANG_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BERUANG_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
