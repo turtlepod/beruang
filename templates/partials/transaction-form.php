f35c96408de7bfb48b2f952629b51139a194d35c
David <david.warna@gmail.com>
2026-06-29 16:25:10 +0700

    fix: align $wallets @var spacing with surrounding doc block

--- templates/partials/transaction-form.php
+++ templates/partials/transaction-form.php
@@ -10,7 +10,7 @@
  * @var string $time         Default time (H:i).
  * @var string $currency     Currency code.
  * @var array  $categories   Flat categories from DB.
- * @var array  $wallets          Wallets from DB.
+ * @var array  $wallets      Wallets from DB.
  * @var int    $default_wallet_id Default wallet ID.
  * @var int    $decimal_places    Number of decimal places.
  */
